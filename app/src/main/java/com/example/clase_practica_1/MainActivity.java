625001bdcff11c849ceee8f784492dae88b22f5a
DaVenSena <dvp@cooweb.co>
2024-03-15 18:05:17 -0500

    changed the way to show the result of the addition and created and changed the activity launcher

--- app/src/main/java/com/example/clase_practica_1/MainActivity.java
+++ app/src/main/java/com/example/clase_practica_1/MainActivity.java
@@ -1,19 +1,25 @@
 package com.example.clase_practica_1;
 
+import android.content.Intent;
+import android.icu.text.Edits;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mpas.R;
+import com.example.mpas.resta;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText edtNum1, edtNum2;
+    TextView txtResult;
 
-    Button btnCalculate;
+    Button btnCalculate, btnSubstract;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +28,40 @@ public class MainActivity extends AppCompatActivity {
 
         edtNum1 = findViewById(R.id.edtNum1);
         edtNum2 = findViewById(R.id.edtNum2);
+        txtResult = findViewById(R.id.txtResult);
 
         btnCalculate = findViewById(R.id.btnCalculate);
+        btnSubstract = findViewById(R.id.btnSubstract);
 
-        btnCalculate.setOnClickListener(view -> {
-            int num1 = Integer.parseInt(edtNum1.getText().toString());
-            int num2 = Integer.parseInt(edtNum2.getText().toString());
-            int sum = num1 + num2;
-            new AlertDialog.Builder(MainActivity.this)
-                    .setTitle("La suma de los numeros " + num1 + " y " + num2 + " es:")
-                    .setMessage(String.valueOf(sum))
-                    .show();
-        });
+        btnCalculate.setOnClickListener(view -> calculate(edtNum1, edtNum2, txtResult));
+        btnSubstract.setOnClickListener(view -> substract());
+
+
+    }
+
+    public void substract() {
+        Intent i = new Intent(MainActivity.this, resta.class);
+        startActivity(i);
+    }
+
+    public void calculate(EditText edtNum1, EditText edtNum2, TextView txtResult) {
+
+        String val1 = edtNum1.getText().toString();
+        String val2 = edtNum2.getText().toString();
+        if (!val1.isEmpty() && !val2.isEmpty()) {
+            int num1 = Integer.parseInt(val1);
+            int num2 = Integer.parseInt(val2);
+
+            txtResult.setText(String.valueOf(addTwoNumbers(num1, num2)));
+        } else {
+            Toast.makeText(MainActivity.this, "Debe poner los numeros a sumar", Toast.LENGTH_LONG).show();
+            txtResult.setText("0");
+            edtNum2.setText("");
+            edtNum1.setText("");
+        }
+    }
+
+    public int addTwoNumbers(int num1, int num2) {
+        return num1 + num2;
     }
 }
